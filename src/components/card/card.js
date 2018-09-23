03cdaafc009ecc106cac6df17b2e7dbc9ae348e2
AAslsop12 <aimee.johansen33@gmail.com>
2018-09-22 20:46:25 -0600

    used JSON to populate the values

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -3,20 +3,35 @@ import Congratulations from './congratulations/congratulations';
 import Category from './category/category';
 
 class Card extends Component {
+
     render() {
-        return (
+
+        var responseArray = {
+            creditscore: 706,
+            creditAge: "16yr" ,
+            inquiries: 2,
+            bankruptcies: 0,
+            creditUtilization: "32%",
+            creditLimit: "49,300",
+
+    };
+
+
+
+         return(
             <div className='card'>
-                <Congratulations />
-                <Category className='card__score' header="Personal Credit Score" value='706'/>
-                <Category className='card__age' header="Personal Credit Age" value='16yr'/>
-                <Category className='card__inquiries' header="Personal Inquiries" value='2'/>
-                <Category className='card__bankruptcies' header="Personal Bankruptcies" value='0'/>
-                <Category className='card__utilization' header="Personal Credit Utilization" value='32%'/>
-                <Category className='card__limit' header="Personal Credit Limit" value='49,300'/>
-      
+            <Congratulations />
+            <Category className='card__score' header="Personal Credit Score" value={responseArray.creditscore}/>
+            <Category className='card__age' header="Personal Credit Age" value={responseArray.creditAge}/>
+            <Category className='card__inquiries' header="Personal Inquiries" value={responseArray.inquiries}/>
+            <Category className='card__bankruptcies' header="Personal Bankruptcies" value={responseArray.bankruptcies}/>
+            <Category className='card__utilization' header="Personal Credit Utilization" value={responseArray.creditUtilization}/>
+            <Category className='card__limit' header="Personal Credit Limit" value={responseArray.creditLimit}/>
+  
             </div>
         )
     }
 }
-
 export default Card;
+
+
